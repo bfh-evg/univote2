39095903e9e0ad434cb7bfce2267502867d3ca01
sevhau <severin.hauser@bfh.ch>
2015-06-17 13:50:57 +0200

    Add ressource bundle

--- component-core/component-core-war/src/main/java/ch/bfh/univote2/component/core/jsf/LoginBean.java
+++ component-core/component-core-war/src/main/java/ch/bfh/univote2/component/core/jsf/LoginBean.java
@@ -45,8 +45,6 @@ import ch.bfh.univote2.component.core.manager.TenantManager;
 import java.io.Serializable;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 
 /**
@@ -75,39 +73,33 @@ public class LoginBean implements Serializable {
 			this.password = null;
 			return "/secured/welcome";
 		}
-		FacesMessage msg = new FacesMessage("Login error!", "ERROR MSG");
-		msg.setSeverity(FacesMessage.SEVERITY_ERROR);
-		FacesContext.getCurrentInstance().addMessage(null, msg);
+		MessageFactory.error("core", "login_error");
 		return "/login";
 	}
 
 	public String doLogout() {
 		this.loggedIn = false;
 		this.password = null;
-		FacesMessage msg = new FacesMessage("Logout success!", "INFO MSG");
-		msg.setSeverity(FacesMessage.SEVERITY_INFO);
-		FacesContext.getCurrentInstance().addMessage(null, msg);
+		MessageFactory.info("core", "logout_success");
 		return "/index";
 
 	}
 
 	public String doLock() {
 		if (!this.tenantManager.lock(username, password)) {
-			FacesMessage msg = new FacesMessage("Can not lock!", "INFO MSG");
-			msg.setSeverity(FacesMessage.SEVERITY_INFO);
-			FacesContext.getCurrentInstance().addMessage(null, msg);
+			MessageFactory.error("core", "locking_error");
 		}
 		this.password = null;
+		MessageFactory.info("core", "locking_success");
 		return null;
 	}
 
 	public String doUnlock() {
 		if (!this.tenantManager.unlock(username, password)) {
-			FacesMessage msg = new FacesMessage("Can not unlock!", "INFO MSG");
-			msg.setSeverity(FacesMessage.SEVERITY_INFO);
-			FacesContext.getCurrentInstance().addMessage(null, msg);
+			MessageFactory.error("core", "unlocking_error");
 		}
 		this.password = null;
+		MessageFactory.info("core", "unlocking_success");
 		return null;
 	}
 
