eb06f729dd521e82b62fc32069807e3742e76d20
Stephan Fischli <stephan.fischli@bfh.ch>
2015-08-10 11:32:15 +0200

    Updated JSON schemas and examples

--- admin-client/src/main/java/ch/bfh/univote/admin/Main.java
+++ admin-client/src/main/java/ch/bfh/univote/admin/Main.java
@@ -12,32 +12,32 @@ import javax.xml.transform.stream.StreamSource;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-	JAXBContext jaxbContext = initJAXBContext(ElectionDetails.class);
-	ElectionDetails electionDetails = unmarshal(jaxbContext,
-		ElectionDetails.class, "/json-examples/sub-2015/electionDetails.json");
-	String jsonString = marshal(jaxbContext, electionDetails);
-	System.out.println(jsonString);
-    }
+	public static void main(String[] args) throws Exception {
+		JAXBContext jaxbContext = initJAXBContext(ElectionDetails.class);
+		ElectionDetails electionDetails = unmarshal(jaxbContext,
+				ElectionDetails.class, "/json-examples/sub-2015/electionDetails.json");
+		String jsonString = marshal(jaxbContext, electionDetails);
+		System.out.println(jsonString);
+	}
 
-    private static <T> JAXBContext initJAXBContext(Class<T> type) throws Exception {
-	Map<String, Object> properties = new HashMap();
-	properties.put("eclipselink.media-type", "application/json");
-	properties.put("eclipselink.json.include-root", false);
-	return JAXBContext.newInstance(new Class[]{type}, properties);
-    }
+	private static <T> JAXBContext initJAXBContext(Class<T> type) throws Exception {
+		Map<String, Object> properties = new HashMap();
+		properties.put("eclipselink.media-type", "application/json");
+		properties.put("eclipselink.json.include-root", false);
+		return JAXBContext.newInstance(new Class[]{type}, properties);
+	}
 
-    private static <T> T unmarshal(JAXBContext jaxbContext, Class<T> type, String path) throws Exception {
-	Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-	InputStream stream = type.getResourceAsStream(path);
-	return unmarshaller.unmarshal(new StreamSource(stream), type).getValue();
-    }
+	private static <T> T unmarshal(JAXBContext jaxbContext, Class<T> type, String path) throws Exception {
+		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+		InputStream stream = type.getResourceAsStream(path);
+		return unmarshaller.unmarshal(new StreamSource(stream), type).getValue();
+	}
 
-    private static String marshal(JAXBContext jaxbContext, Object object) throws Exception {
-	StringWriter writer = new StringWriter();
-	Marshaller marshaller = jaxbContext.createMarshaller();
-	marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-	marshaller.marshal(object, writer);
-	return writer.toString();
-    }
+	private static String marshal(JAXBContext jaxbContext, Object object) throws Exception {
+		StringWriter writer = new StringWriter();
+		Marshaller marshaller = jaxbContext.createMarshaller();
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		marshaller.marshal(object, writer);
+		return writer.toString();
+	}
 }
