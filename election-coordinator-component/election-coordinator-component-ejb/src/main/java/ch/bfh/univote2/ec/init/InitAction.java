dc24dfc7764e494a2ca231e7b42e4947fa6fc161
Eric Dubuis <eric.dubuis@bfh.ch>
2015-08-10 18:14:06 +0200

    Polished class.

--- election-coordinator-component/election-coordinator-component-ejb/src/main/java/ch/bfh/univote2/ec/init/InitAction.java
+++ election-coordinator-component/election-coordinator-component-ejb/src/main/java/ch/bfh/univote2/ec/init/InitAction.java
@@ -82,7 +82,7 @@ public class InitAction extends AbstractAction implements NotifiableAction {
 		InitUserInput userInput = (InitUserInput) notification;
 		ActionContextKey ack = new ActionContextKey(ACTION_NAME, actionContext.getActionContextKey().getTenant(),
 				userInput.getSection());
-		ActionContext newContext = new InitActionContext(ack, null);
+		ActionContext newContext = new InitActionContext(ack, new ArrayList<>());
 		this.informationService.informTenant(ACTION_NAME, actionContext.getActionContextKey().getTenant(),
 				userInput.getSection(), "New course of action defined for section: " + userInput.getSection());
 		this.actionManager.runFinished(newContext, ResultStatus.FINISHED);
